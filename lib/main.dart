85af5254e01d7219cc5295caa9b7d769f0f08a7d
Anom Maharjan <anmmzn1@gmail.com>
2021-01-08 10:38:39 +0545

    Features added

--- lib/main.dart
+++ lib/main.dart
@@ -4,14 +4,34 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'Starwars Characters',
+    home: SafeArea(
+      bottom: true,
+          child: Scaffold(
+            backgroundColor: Colors.black,
+        appBar: AppBar(
+          backgroundColor: Colors.black,
+          centerTitle: true,
+          title: Text(
+            'Starwars Characters',
+            style: TextStyle(
+              color: Colors.yellow,
+              fontFamily: 'Starjedi'
+            ),
+            ),
+        ),
+        body: StarwarsPage(),
+        
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.black,
+          shape: CircleBorder(),
+          tooltip: 'Next Page',
+          child: Icon(
+          Icons.navigate_next,
+          color: Colors.yellow,
           ),
-      ),
-      body: StarwarsPage(),
-      ),
+        ),
+        ),
+    ),
   ));
 }
 
--- lib/pages/starwars_page.dart
+++ lib/pages/starwars_page.dart
@@ -44,36 +44,65 @@ class StarwarsPage extends StatelessWidget {
   Widget buildSuccessWidget() {
     // final character = controller.characters;
     List<dynamic> names = controller.characters.results;
-    return SingleChildScrollView(
-      child: Column(children: List.generate(names.length, (i){
-        return Container(
-          child: Card(
-            child: ListTile(
-              title: Text(
-                "Name: ${names[i]['name']}",
-                style: TextStyle(color: Colors.red),
-              ),
-              subtitle: Text(
-                "Gender: ${names[i]['gender']}"
-              ),
+    return Container(
+      decoration: BoxDecoration(
+      // color: Colors.black,
+      image: DecorationImage(
+        image: AssetImage("images/starwars.jpeg"), 
+        fit: BoxFit.cover,
+      )
+      ),
+          child: GridView.count(
+              crossAxisCount: 2,
+        children: List.generate(names.length, (i) {
+          return Container(
+             decoration: BoxDecoration(
+        borderRadius: BorderRadius.all(Radius.circular(10)),
+      color: Colors.yellow,
+        ),
+            margin: EdgeInsets.symmetric(vertical: 10, horizontal: 5),
+                    child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                Text(
+                  "Name: ${names[i]['name']}",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 14,
+                    fontFamily: 'StarJedi'
+                  ),
+                ),
+                Text(
+                  "Gender: ${names[i]['gender']}",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 13,
+                    fontFamily: 'StarJedi'
+                  ),
+                ),
+                Text(
+                  "Height: ${names[i]['height']}",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 13,
+                    fontFamily: 'StarJedi'
+                  ),
+                ),
+                Text(
+                  "Weight: ${names[i]['mass']}",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 13,
+                    fontFamily: 'StarJedi'
+                  ),
+                ),
+              ],
             ),
-          ),
-        );
-      } ),
-      // color: Colors.blueAccent,
-      // padding: EdgeInsets.symmetric(vertical: 20, horizontal: 10),
-      // child: new Card(
-      //   child: ListTile(
-      //     title: Text(
-      //       "${names[0]['name']}",
-      //       style: new TextStyle(
-      //         fontSize: 20,
-      //         color: Colors.red,
-      //       ),
-      //     ),
-      //   ),
-      // ),
-
-    ));
+          );
+        }
+        ),
+        
+      ),
+    );
   }
 }
